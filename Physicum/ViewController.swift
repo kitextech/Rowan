f1b29b5a6623278bfe6a6315b94bf0335c1830b3
GreatApe <kugel@kth.se>
2017-06-07 18:42:03 +0200

    Initial Commit

---
+++ Physicum/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Physicum
+//
+//  Created by Gustaf Kugelberg on 2017-06-07.
+//  Copyright © 2017 Gustaf Kugelberg. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
