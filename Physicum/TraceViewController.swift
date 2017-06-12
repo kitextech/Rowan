13821a5d65327ae6f3ff8e146cd67a9092a1903e
GreatApe <kugel@kth.se>
2017-06-12 23:21:50 +0200

    physics works!

--- Physicum/TraceViewController.swift
+++ Physicum/TraceViewController.swift
@@ -16,17 +16,65 @@ class TraceViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        var ball = BallDrawable()
-        ball.position = Vector(0, 0, -25)
-        traceView.add(ball)
+        let ball1 = BallDrawable(position: 20*e_z)
+        let ball2 = BallDrawable(position: 15*e_z)
+        let ball3 = BallDrawable(position: 10*e_z)
+        let ball4 = BallDrawable(position: 5*e_z)
+        let ball5 = BallDrawable(position: 0*e_z)
+        let ball6 = BallDrawable(position: -5*e_z)
+        let ball7 = BallDrawable(position: -10*e_z)
+        let ball8 = BallDrawable(position: -15*e_z)
+        let ball9 = BallDrawable(position: -20*e_z)
+        let ball10 = BallDrawable(position: -25*e_z)
 
-        let ballObject = Object(m: 10, r: ball.position, v: 10*e_y, a: .zero)
-        newton.add(object: ballObject, id: ball.id)
-//        newton.add(force: gravity, id: ball.id)
-        newton.add(force: radial, id: ball.id)
+        traceView.add(ball1)
+        traceView.add(ball2)
+        traceView.add(ball3)
+        traceView.add(ball4)
+        traceView.add(ball5)
+        traceView.add(ball6)
+        traceView.add(ball7)
+        traceView.add(ball8)
+        traceView.add(ball9)
+        traceView.add(ball10)
+
+        let ballObject1 = Object(m: 1, r: ball1.position, v: -5*e_z, a: .zero)
+        let ballObject2 = Object(m: 1, r: ball2.position, v: 0*e_z, a: .zero)
+        let ballObject3 = Object(m: 1, r: ball3.position, v: 0*e_z, a: .zero)
+        let ballObject4 = Object(m: 1, r: ball4.position, v: 0*e_z, a: .zero)
+        let ballObject5 = Object(m: 1, r: ball5.position, v: 0*e_z, a: .zero)
+        let ballObject6 = Object(m: 1, r: ball6.position, v: 0*e_z, a: .zero)
+        let ballObject7 = Object(m: 1, r: ball7.position, v: 0*e_z, a: .zero)
+        let ballObject8 = Object(m: 1, r: ball8.position, v: 0*e_z, a: .zero)
+        let ballObject9 = Object(m: 1, r: ball9.position, v: 0*e_z, a: .zero)
+        let ballObject10 = Object(m: 1, r: ball10.position, v: 0*e_z, a: .zero)
+
+        newton.add(object: ballObject1, id: ball1.id)
+        newton.add(object: ballObject2, id: ball2.id)
+        newton.add(object: ballObject3, id: ball3.id)
+        newton.add(object: ballObject4, id: ball4.id)
+        newton.add(object: ballObject5, id: ball5.id)
+        newton.add(object: ballObject6, id: ball6.id)
+        newton.add(object: ballObject7, id: ball7.id)
+        newton.add(object: ballObject8, id: ball8.id)
+        newton.add(object: ballObject9, id: ball9.id)
+        newton.add(object: ballObject10, id: ball10.id)
+
+        newton.add(mutualForce: spring, id0: ball1.id, id1: ball2.id)
+        newton.add(mutualForce: spring, id0: ball2.id, id1: ball3.id)
+        newton.add(mutualForce: spring, id0: ball3.id, id1: ball4.id)
+        newton.add(mutualForce: spring, id0: ball4.id, id1: ball5.id)
+        newton.add(mutualForce: spring, id0: ball5.id, id1: ball6.id)
+        newton.add(mutualForce: spring, id0: ball6.id, id1: ball7.id)
+        newton.add(mutualForce: spring, id0: ball7.id, id1: ball8.id)
+        newton.add(mutualForce: spring, id0: ball8.id, id1: ball9.id)
+        newton.add(mutualForce: spring, id0: ball9.id, id1: ball10.id)
+
+//        newton.add(force: wind, id: ball.id)
+//        newton.add(force: radial, id: ball1.id)
 
         traceView.add(SphereDrawable())
-        traceView.add(BallDrawable())
+//        traceView.add(BallDrawable())
 
         startDisplayLink()
     }
@@ -46,23 +94,12 @@ class TraceViewController: UIViewController {
 
     private func startDisplayLink() {
         stopDisplayLink()
-
-//        startTime = CACurrentMediaTime()
-
         displayLink = CADisplayLink(target: self, selector: #selector(step))
         displayLink?.add(to: .main, forMode: .commonModes)
     }
 
     @objc func step(link: CADisplayLink) {
-//        let elapsed = CACurrentMediaTime() - startTime
-//
-//        guard elapsed < animLength else {
-//            stopDisplayLink()
-//            return
-//        }
-//
-
-        updatePhysics(link.targetTimestamp - link.timestamp)
+        updatePhysics(6*(link.targetTimestamp - link.timestamp))
     }
 
     private func updatePhysics(_ elapsed: TimeInterval) {
